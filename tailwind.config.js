0f55279844bfcc7d2c99a8bca6cde61505f05603
sachini <sbamarasinghe97@gmail.com>
2024-02-18 15:18:54 +0530

    Added Profile page structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            // backgroundImage: {
+            //     'profile-background': "url('background.png')",
+            //   },
+        },
     },
     plugins: [],
 }
